14305528d11b74150ffde71ce122c9681f5d1ac5
ymnkcrcr <ymnkcrcr@users.noreply.github.com>
2026-03-16 12:12:39 +0000

    deploy: 7572ca49c5a78d512a67801436ed174e1a16dfaa

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyDCJiptgNCdQYTsuFlA--Eaoa3TOx_5Fgs",
+  authDomain:        "yyportfolio-b03d5.firebaseapp.com",
+  databaseURL:       "https://yyportfolio-b03d5-default-rtdb.firebaseio.com",
+  projectId:         "yyportfolio-b03d5",
+  storageBucket:     "yyportfolio-b03d5.firebasestorage.app",
+  messagingSenderId: "491963503933",
+  appId:             "1:491963503933:web:d235cb2903fc2fb9882154"
+};
